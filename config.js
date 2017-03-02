a33d9c7b099996c289f3e66790edfe882ee6ae58
yashshiroya <yshiroya@purdue.edu>
2017-03-02 02:01:31 -0500

    Pushing final bot

--- config.js
+++ config.js
@@ -8,12 +8,3 @@ const config = {
 module.exports = config;
 
 
-curl -X POST -H "Content-Type: application/json" -d '{
-  "setting_type":"call_to_actions",
-  "thread_state":"new_thread",
-  "call_to_actions":[
-    {
-      "payload":"new_user"
-    }
-  ]
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAADkTpZBZBeH8BAEV7zpeT5gUeSuOhM6Igno58JIpcVmONvbNSc7Rdo0ZAIrkQmJz4fDXJX90uZAFPKqb6mdyKElkO5uEqKfeH2FD85ZBAk3QLfLCxAcAOWZCMVHnKeBsrlJSTqouVhWkMfh40M88ajKqJS4fy6G9fkwR20VEiRgZDZD"
